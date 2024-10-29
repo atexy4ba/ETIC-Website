efcbb2c2bf947a5f76de5be04b60561420f1531a
Zakariaaben <zakaria.benhamiche@gmail.com>
2024-10-29 22:18:07 +0100

    Navigation Bar v1 w/ framer-motion animation

---
+++ src/components/Navbar/ContactButton.jsx
@@ -0,0 +1,9 @@
+export const ContactButton = () => {
+  return (
+    <>
+      <button className="bg-[#20B3AD]  py-[10px] px-6 rounded-full font-semibold text-white max-md:hidden">
+        Contactez-nous
+      </button>
+    </>
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import localFont from "next/font/local";
+import { Navbar } from "@/components/Navbar/Navbar";
 
 // const satoshiRegular = localFont({
 //   src: "./fonts/Satoshi-Regular.woff",
@@ -9,6 +10,8 @@ import localFont from "next/font/local";
 
 export default function Home() {
   return (
-    <div className="font-satoshi font-bold text-8xl uppercase">Bon courage</div>
+    <div className="font-satoshi font-bold text-8xl ">
+      <Navbar/>
+    </div>
   );
 }
