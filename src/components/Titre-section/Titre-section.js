f1bd7ec304314bbd26bdbc0dc4f46fce0b59db00
AdlanAdi <na_adi@esi.dz>
2024-10-29 19:03:22 +0100

    HeroSection + TitreSection

---
+++ src/components/Titre-section/Titre-section.js
@@ -0,0 +1,23 @@
+
+import PropTypes from 'prop-types';
+
+function TitreSection(props) { 
+    return ( 
+        <div className='flex flex-row justify-evenly  bg-white bg-opacity-100 m-5'>
+            <h1 className='sm:text-2xl md:text-3xl lg:text-5xl font-bold font-satoshi '>
+                {props.h1}
+                <span className='text-blue'> {props.span}</span>
+            </h1>
+            <p className='text-xs md:text-xs lg:text-lg font-satoshi block  text-black '>{props.p}</p>
+        </div>
+    );
+}
+
+// Define prop types for validation
+TitreSection.propTypes = {
+    h1: PropTypes.string.isRequired,  // h1 must be a string and is required
+    span: PropTypes.string.isRequired, // span must be a string and is required
+    p: PropTypes.string.isRequired,    // p must be a string and is required
+};
+
+export default TitreSection;
