1c8a4166ad940001cf719b917f6de858ea9a14a9
aniss <ka_bessalah@esi.dz>
2024-12-26 20:45:18 +0100

    conatct

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div className="flex md:flex-row flex-col justify-between text-xl gap-8 px-5 md:px-24">
+          <div className="flex md:w-[40%] flex-col justify-between gap-8">
+            <div className="flex flex-col gap-4">
+              <h1 className="md:text-6xl text-4xl font-bold font-satoshi">Get in touch <br /> <span className="text-blue">with ETIC</span></h1>
+              <p className="font-normal text-sm md:text-xl">
+                Vous voulez collaborer avec ETIC ? Contactez nous en remplissant
+                tout simplement le formulaire.
+              </p>
+            </div>
+    
+            <div className="flex flex-col md:gap-4 gap-2">
+                <h2 className="md:text-2xl">Nos coordonnées</h2>
+                <div className="flex flex-col text-sm md:text-xl">
+                    <p className="font-normal">etic@esi.dz</p>
+                    <p className="font-normal">+213 551 12 50 25</p>
+                    <p className="font-normal">Ecole Nationale Supérieure d'Informatique, Oued Smar, Alger</p>
+                </div>
+                
+            </div>
+          </div>
+    
+          <div className="md:w-[50%] text-[#2D2D2D] text-sm md:text-xl">
+            <form className="flex flex-col text-sm md:text-xl gap-4 md:gap-8">
+              <div className="flex flex-col">
+                <label htmlFor="name">Nom et prénom</label>
+                <input type="text" id="name" className="p-2 bg-[#FAFAFA] rounded-md border-[#D9D9D9] border-2"/>
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="name">E-mail*</label>
+                <input type="text" required id="name" className="p-2 bg-[#FAFAFA] rounded-md border-[#D9D9D9] border-2"/>
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="message">Message</label>
+                <textarea id="message" className="p-2 font-normal bg-[#FAFAFA] rounded-md border-[#D9D9D9] border-2" rows="4"></textarea>
+              </div>
+              <button type="submit" className="bg-blue py-4 rounded-full text-white">Envoyer</button>
+            </form>
+          </div>
+        </div>
+      );
+}
+
+export default Contact;
+
+
+
+
