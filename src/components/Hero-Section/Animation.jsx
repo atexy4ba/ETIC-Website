6c2395e2471357f96e5fc14e80e5a52ae65cac4f
Bouchbout-sami <ks_bouchbout@esi.dz>
2024-12-29 16:55:53 +0100

    links tae navbar + raffinement mais had khetra droite guache

--- src/components/Hero-Section/Animation.jsx
+++ src/components/Hero-Section/Animation.jsx
@@ -1,33 +1,21 @@
 import React from "react";
 import Lottie from "react-lottie";
-import * as animationData from "./animation.json";
+import animation from "./animation.json";
 
-export default class LottieControl extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { isStopped: false, isPaused: false, isClient: false };
-  }
+const Animation = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+  return (
+    <div className="h-12 md:h-auto z-0">
+      <Lottie options={defaultOptions} />
+    </div>
+  );
+};
 
-  componentDidMount() {
-    this.setState({ isClient: true });
-  }
-
-  render() {
-    if (!this.state.isClient) return null;
-
-    const defaultOptions = {
-      loop: true,
-      autoplay: true,
-      animationData: animationData,
-      rendererSettings: {
-        preserveAspectRatio: "xMidYMid slice",
-      },
-    };
-
-    return (
-      <div className="h-12 md:h-auto z-0">
-        <Lottie options={defaultOptions} />
-      </div>
-    );
-  }
-}
+export default Animation;
