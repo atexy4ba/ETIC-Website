d0eb06b840b21075854f068f5d7d4e7e678fe9ba
Bouchbout-sami <ks_bouchbout@esi.dz>
2024-12-29 16:45:38 +0100

    links tae navbar + raffinement

--- src/components/Hero-Section/Animation.jsx
+++ src/components/Hero-Section/Animation.jsx
@@ -5,10 +5,16 @@ import * as animationData from "./animation.json";
 export default class LottieControl extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { isStopped: false, isPaused: false };
+    this.state = { isStopped: false, isPaused: false, isClient: false };
+  }
+
+  componentDidMount() {
+    this.setState({ isClient: true });
   }
 
   render() {
+    if (!this.state.isClient) return null;
+
     const defaultOptions = {
       loop: true,
       autoplay: true,
@@ -16,12 +22,11 @@ export default class LottieControl extends React.Component {
       rendererSettings: {
         preserveAspectRatio: "xMidYMid slice",
       },
-      isClickToPauseDisabled: true,
     };
 
     return (
       <div className="h-12 md:h-auto z-0">
-        {document ? <Lottie options={defaultOptions} /> : null}
+        <Lottie options={defaultOptions} />
       </div>
     );
   }
